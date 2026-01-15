5d6ed54f2573c3bc5440849f0a0bea7e02f11fb4
Seth Adkins <seth.adkins@protonmail.com>
2026-01-14 21:53:19 -0500

    use stdenv.hostPlatform.system instead of system

--- flake.nix
+++ flake.nix
@@ -53,7 +53,7 @@
 
               package = mkOption {
                 type = types.package;
-                default = self.packages.${pkgs.system}.default;
+                default = self.packages.${pkgs.stdenv.hostPlatform.system}.default;
                 description = "The openrgb-effects package to use";
               };
             };
